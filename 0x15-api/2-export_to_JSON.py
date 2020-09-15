be26335674b015bf840495528a73e3891e87dd15
cmillecan <cmillecan@gmail.com>
2020-09-15 03:27:01 +0000

    add file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,11 +7,10 @@ if __name__ == '__main__':
     import sys
     import csv
 
-    user_id = sys.arg[1]
+    user_id = sys.argv[1]
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
                         format(user_id))
-
-    username = user.json()[0].get('username')
+    username = user.json().get('username')
     todos = requests.get('https://jsonplaceholder.typicode.com/todos/',
                          params={'userId': user_id})
     todos = todos.json()
@@ -25,6 +24,5 @@ if __name__ == '__main__':
         t['task'] = tasks.get('title')
         t['completed'] = tasks.get('completed')
         employee[user_id].append(t)
-
     with open('{}.json'.format(user_id), 'w') as f:
         json.dump(employee, f)

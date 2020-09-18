f856248e340ad38b89ac8fb373feb82d3e9219ad
cmillecan <cmillecan@gmail.com>
2020-09-18 04:53:26 +0000

    add file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns number of subscribers"""
-    r = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
             headers={'User-Agent': 'custom'}, allow_redirects=False)
     if r.status_code != 200:
         return 0

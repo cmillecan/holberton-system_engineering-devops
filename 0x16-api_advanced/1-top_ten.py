3cec0f24d4489d1e69a5161e83d9e19aa61b005d
cmillecan <cmillecan@gmail.com>
2020-09-18 05:00:36 +0000

    add file

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of top 10 posts"""
+    r = request.get('https://www.reddit.com/r/{}/hot.json?limit=10'.
+                    format(subreddit), headers={'User-Agent': 'custom'},
+                    allow_redirects=False)
+    if r.status_code == 200:
+        for key in r.json().get('data').get('children'):
+            print(key.get('data').get('title'))
+    else:
+        print(None)

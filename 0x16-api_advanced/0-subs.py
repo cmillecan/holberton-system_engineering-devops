81d45d9e796fc6fbf5fa3c06a791e5610386789e
cmillecan <cmillecan@gmail.com>
2020-09-18 04:41:05 +0000

    add file

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+"""
+def number_of_subscribers(subreddit):
+    """Returns number of subscribers"""
+    r = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+            headers={'User-agen': 'custom'}, allow_redirects=False)
+    if r.status_code != 200:
+        return 0
+    return r.json().get('data').get('subscribers')
